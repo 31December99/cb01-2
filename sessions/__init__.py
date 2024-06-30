58be01145ea3a56ffc0b8febc6d87fa130be8df0
Parzival <jw79985@gmail.com>
2024-06-30 23:15:03 +0200

    Add new class

--- sessions/__init__.py
+++ sessions/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from sessions.session_cached import MyHttp, Cache
+from sessions.session import MyHttp, Cache
 from sessions.stay import Stayonline
 from sessions.agents import Agent, user_agents
 from sessions.truelink import true_link
+from sessions.page import Page
+from sessions.split import Split
