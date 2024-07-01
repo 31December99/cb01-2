59ae1fcf626f0f83f6e317fdd07269dc21539523
Parzival <jw79985@gmail.com>
2024-07-02 00:46:42 +0200

    Add refact

--- start.py
+++ start.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import asyncio
+import sys
+
 from sessions import true_link, Page, Split
 from download import Downloader
 
@@ -22,73 +24,31 @@ async def start():
         mixdrop_player_url = page.mixdrop_url(stayonline_url=best_resolution_url)
         print(f"{mixdrop_player_url}...Attendi generazione dei link per il download")
 
-        proxy_group = ['http://104.207.59.91:3128', 'http://104.207.42.108:3128',
-                       'http://104.207.49.179:3128', 'http://104.207.58.16:3128',
-                       'http://104.207.52.104:3128', 'http://104.207.60.98:3128',
-                       'http://104.207.47.249:3128', 'http://104.207.44.136:3128',
-                       'http://104.207.37.94:3128', 'http://104.167.25.177:3128',
+        proxy_group = ['http://104.207.52.23:3128', 'http://104.167.25.171:3128',
+                       'http://104.207.51.28:3128', 'http://104.167.26.142:3128',
+                       'http://104.167.26.202:3128', 'http://104.167.31.121:3128',
+                       'http://104.167.29.55:3128', 'http://104.207.36.135:3128',
+                       'http://104.167.31.88:3128', 'http://104.167.29.54:3128',
                        None]  # None = Ip corrente
 
-        # Url stayonline con proxy #0
-        respond, php_sessid0 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[0])
-        download_url0 = true_link(respond)
+        download_group = []
+        php_sessid_group = []
 
-        # Url stayonline con proxy #1
-        respond, php_sessid1 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[1])
-        download_url1 = true_link(respond)
-
-        # Url stayonline con proxy #2
-        respond, php_sessid2 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[2])
-        download_url2 = true_link(respond)
-
-        # Url stayonline con proxy #3
-        respond, php_sessid3 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[3])
-        download_url3 = true_link(respond)
-
-        # Url stayonline con proxy #4
-        respond, php_sessid4 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[4])
-        download_url4 = true_link(respond)
-
-        # Url stayonline con proxy #5
-        respond, php_sessid5 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[5])
-        download_url5 = true_link(respond)
-
-        # Url stayonline con proxy #6
-        respond, php_sessid6 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[6])
-        download_url6 = true_link(respond)
-
-        # Url stayonline con proxy #7
-        respond, php_sessid7 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[7])
-        download_url7 = true_link(respond)
-
-        # Url stayonline con proxy #8
-        respond, php_sessid8 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[8])
-        download_url8 = true_link(respond)
-
-        # Url stayonline con proxy #9
-        respond, php_sessid9 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[9])
-        download_url9 = true_link(respond)
-
-        # Url stayonline con proxy #10
-        respond, php_sessid10 = await page.get_url(url=mixdrop_player_url, proxy=proxy_group[10])
-        download_url10 = true_link(respond)
-
-        # Url stayonline con proxy #11
-        respond, php_sessid11 = await page.get_url(url=mixdrop_player_url)
-        download_url11 = true_link(respond)
-
-        download_group = [download_url0, download_url1, download_url2, download_url3, download_url4,
-                          download_url5, download_url6, download_url7, download_url8, download_url9,
-                          download_url10, download_url11]
-
-        php_sessid_group = [php_sessid0, php_sessid1, php_sessid2, php_sessid3, php_sessid4,
-                            php_sessid5, php_sessid6, php_sessid7, php_sessid8, php_sessid9,
-                            php_sessid10, php_sessid11]
+        for proxy_ip in proxy_group:
+            respond, php_sessid = await page.get_url(url=mixdrop_player_url, proxy=proxy_ip)
+            download_url = true_link(respond)
+            download_group.append(download_url)
+            php_sessid_group.append(php_sessid)
+            if download_url:
+                print(f"Real link:{download_url}  Sessid:{php_sessid}")
+            else:
+                print("Url non disponibile")
+                sys.exit()
 
         # Splitto il file in tre parti uno per proxy e passi i gli url delivery
         # infine richiedo con HEAD il content-lenght e divido il size per il numero di parti richieste
-        sp = Split(download_url11)
-        headers_ranges = sp.get_ranges(parts=11, download_group=download_group,
+        sp = Split(download_group[-1])
+        headers_ranges = sp.get_ranges(parts=len(proxy_group), download_group=download_group,
                                        proxy_group=proxy_group, php_sessid_group=php_sessid_group)
 
         # Passo tutto al downloader
